de9bb251a62365af8d6b6ad3a567a08d8f710232
TateBennet <tate.van_de_bennet@lethpolytech.ca>
2025-11-18 18:15:35 -0700

    fully assembled final build
    completed final build with new content. minor tweaks to be made but everything should be in a stable state.

--- SHE/Assets/tate/scripts/AudioMover.cs
+++ SHE/Assets/tate/scripts/AudioMover.cs
@@ -2,33 +2,71 @@ using UnityEngine;
 
 public class AudioMover : MonoBehaviour
 {
-    public AudioSource targetSource;
-    public AudioSource targetSource2;
+    public AudioSource coach;
+    public AudioSource coachCD;
+    public AudioSource headCoach;
+    public AudioSource guys;
     public Vector3 newPosition; // set in Inspector
     public Vector3 newPosition2;
+    public Vector3 newPosition3;
+    public Vector3 newPosition4;
+    public Vector3 newPosition5;
 
     public void MoveTo()
     {
-        if (targetSource == null)
+        if (coach == null)
         {
             Debug.LogWarning("MoveAudioSource: No AudioSource assigned!");
             return;
         }
 
-        targetSource.transform.position = newPosition;
+        coach.transform.position = newPosition;
     }
 
     public void MoveTo2()
     {
-        if (targetSource == null)
+        if (headCoach == null)
         {
             Debug.LogWarning("MoveAudioSource: No AudioSource assigned!");
             return;
         }
 
-        targetSource2.transform.position = newPosition2;
+        headCoach.transform.position = newPosition2;
+    }
+
+    public void MoveTo3()
+    {
+        if (coachCD == null)
+        {
+            Debug.LogWarning("MoveAudioSource: No AudioSource assigned!");
+            return;
+        }
+
+        coachCD.transform.position = newPosition3;
+    }
+
+    public void MoveTo4()
+    {
+        if (guys == null)
+        {
+            Debug.LogWarning("MoveAudioSource: No AudioSource assigned!");
+            return;
+        }
+
+        guys.transform.position = newPosition4;
+    }
+
+    public void MoveTo5()
+    {
+        if (headCoach == null)
+        {
+            Debug.LogWarning("MoveAudioSource: No AudioSource assigned!");
+            return;
+        }
+
+        headCoach.transform.position = newPosition5;
     }
 
     // Optional utility for scripting
-    public void MoveTo(Vector3 pos) => targetSource.transform.position = pos;
+    public void MoveTo(Vector3 pos) => coach.transform.position = pos;
 }
